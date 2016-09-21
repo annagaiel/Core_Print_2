c6735458607df491fd811d52072c5711a9767180
Anna Natorilla <annagaiel@gmail.com>
2016-09-21 06:19:25 -0700

    initial commit

---
+++ src/two/Print.java
@@ -0,0 +1,15 @@
+package two;
+
+public class Print {
+/**
+ * Prints string with no semicolon
+ * @param args
+ */
+	public static void main(String[] args) {
+		if (System.out.printf("Anna N.", args) == null){
+			
+		}
+
+	}
+
+}
